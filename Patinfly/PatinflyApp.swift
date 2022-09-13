cafd93f111cbd8ce113df7e19010c6a07e35a415
Gerard Serra <100229800+GerardSerra18@users.noreply.github.com>
2022-09-13 19:42:48 +0200

    Initial Commit

---
+++ Patinfly/PatinflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatinflyApp.swift
+//  Patinfly
+//
+//  Created by Gerard Serra Rodríguez on 13/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PatinflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
