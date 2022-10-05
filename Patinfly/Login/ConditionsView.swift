ae700c7d1edcb08d8e93485268146129ff20bb58
Gerard Serra <100229800+GerardSerra18@users.noreply.github.com>
2022-10-05 18:36:23 +0200

    conditions

--- Patinfly/Login/ConditionsView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ConditionsView.swift
-//  Patinfly
-//
-//  Created by Gerard Serra Rodríguez on 5/10/22.
-//
-
-import Foundation
--- Patinfly/Login/LoginView.swift
+++ Patinfly/Login/LoginView.swift
@@ -13,36 +13,43 @@ struct LoginView: View {
     @StateObject private var loginViewModel = LoginViewModel()
     @EnvironmentObject var authentication: Authentication
     @State private var ToogleActivado = true
-    @State private var selection: String? = nil
+    //@State private var selection: String? = nil
     
     var body: some View {
-        VStack {
-            HStack{
-                Image("IniciApp")
-                Divider()
-
-            }.padding()
-            Text("Patinfly").font(.largeTitle).foregroundColor(.blue)
-            TextField("EMAIL", text: $loginViewModel.credentials.email).keyboardType(.emailAddress)
-            
-            SecureField("PASSWORD", text: $loginViewModel.credentials.password)
-            if loginViewModel.showProgressView{
-                ProgressView()
-            }
-            Toggle("Conditions", isOn: $ToogleActivado).padding()
-            Button("Login"){
-                loginViewModel.login{
-                    success in
-                    authentication.updateValidation(success: success)
+        NavigationView{
+            VStack {
+                HStack{
+                    Image("IniciApp")
+                    Divider()
+                    
+                }.padding()
+                Text("Patinfly").font(.largeTitle).foregroundColor(.blue)
+                TextField("EMAIL", text: $loginViewModel.credentials.email).keyboardType(.emailAddress)
+                
+                SecureField("PASSWORD", text: $loginViewModel.credentials.password)
+                if loginViewModel.showProgressView{
+                    ProgressView()
+                }
+                Toggle("Conditions", isOn: $ToogleActivado).padding()
+                Button("Login"){
+                    loginViewModel.login{
+                        success in
+                        authentication.updateValidation(success: success)
+                    }
+                }
+                .disabled(loginViewModel.loginDisable).padding(20)
+                .disabled(!ToogleActivado)
+                
+                /*NavigationView{
+                 NavigationLink(destination: EmptyView()) {
+                 Text("Check Conditions")
+                 }
+                 }*/
+                
+                NavigationLink(destination: ContentViewB()){
+                    Text("Check Conditions")
                 }
             }
-            .disabled(loginViewModel.loginDisable).padding(20)
-            .disabled(!ToogleActivado)
-            
-            Button("Check Conditions"){
-                //Realizar el enlaze con un navigation link or navigation view
-            }
-            
             
         }.padding(.horizontal,60)
             .padding(.vertical,20)
@@ -63,3 +70,15 @@ struct LoginView_Previews: PreviewProvider {
         }
     }
 }
+struct ContentViewB : View {
+        var body: some View {
+            NavigationView {
+                VStack {
+                    Text("MIT License Copyright © 2015-2020 Mark Callow, Khronos Group Inc, and Contributors to the KTX project Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the Software), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice (including the next paragraph) shall be included in all copies or substantial portions of the Software.")
+
+                }.navigationBarTitle("TERMS AND CONDITIONS")
+                .navigationBarHidden(true)
+                .padding()
+            }
+        }
+    }
